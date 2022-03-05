e1814f88fbc0fabc5c760ae1886bff7e8d1a5d04
cameron cyr <cameronjcyr@gmail.com>
2022-03-04 18:44:49 -0600

    json docs building successfully

--- BookStoreScraper.py
+++ BookStoreScraper.py
@@ -3,10 +3,21 @@ from bs4 import BeautifulSoup
 import json
 from datetime import datetime
 
-
-def getBookDocument(url):
+def getGenresAndURLs(url):
     resp = requests.get(url)
     soup = BeautifulSoup(resp.content, "html.parser")
+    sidebar = soup.find('div', {'class': 'side_categories'}).find('ul').find('ul')
+    urlAndGenre = []
+
+    for i in sidebar.find_all('li'):
+        urlAndGenre.append((i.find('a').contents[0].strip(), 'https://books.toscrape.com/' + i.find('a')['href']))
+
+    return urlAndGenre
+
+def getBookDocument(genre, url):
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.content, "html.parser")
+    listOfJSON = [] ##temporary 
 
     for section in soup.find('ol').find_all('li'):
         for a in section.find('h3'):
@@ -21,8 +32,8 @@ def getBookDocument(url):
         }
         starRating = section.find('p')['class'][1]
         rating = ratingMapping[starRating]
-
         prodPriceClass = section.find('div', {'class': 'product_price'})
+
         for p in prodPriceClass.find('p', {'class': 'price_color'}):
             bookPrice = p[1::]
         
@@ -31,32 +42,26 @@ def getBookDocument(url):
         bookJson = json.dumps(
             {
                 'Title': bookName,
+                'Genre': genre,
                 'Rating': rating,
                 'Price (GBP)': bookPrice,
                 'In-Stock Status': prodInStock,
                 'Scrape Date': datetime.now().isoformat()
             }
         )
+
+        listOfJSON.append(bookJson)
     
-    return bookJson
+    return listOfJSON
 
+def main():
+    homePageURL = 'https://books.toscrape.com/'
+    genresURLsTuples = getGenresAndURLs(homePageURL)
+    allBooks = []
 
+    for i in genresURLsTuples:
+        allBooks.append((i[0], getBookDocument(i[0], i[1])))
+        
 
-
-
-# for url in url_list: 
-#     do the thing above ^^^
-
-def getURLList(url):
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.content, "html.parser")
-
-    sidebar = soup.find('div', {'class': 'side_categories'}).find('ul').find('ul')
-
-    for i in sidebar.find_all('li'):
-        print(i.find('a')['href'])
-        print(i.find('a').contents[0].strip())
-
-    return sidebar.prettify()
-
-print(getURLList('https://books.toscrape.com/index.html'))
+if __name__ == "__main__":
+    main()
